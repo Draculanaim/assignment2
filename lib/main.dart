ca8491384bc1d47b2470dc7a35740e133c2d8307
Naim <jimkhannaim97@gmail.com>
2024-11-15 21:57:04 +0600

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -29,59 +29,58 @@ class HomeActivity extends StatelessWidget{
         backgroundColor: Colors.amber,
         //toolbarHeight: ,
         actions: [
-
           IconButton(onPressed: (){}, icon: Icon(Icons.add)),
           IconButton(onPressed: (){}, icon: Icon(Icons.settings)),
           IconButton(onPressed: (){}, icon: Icon(Icons.call)),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Center(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 90,
-                child: Icon(Icons.icecream_outlined,size: 120,),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text('Ice Cream is very delicious right ?',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CircleAvatar(
-                radius: 90,
-                child: Icon(Icons.code,size: 120,),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text('Programming is not boring if you love it',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CircleAvatar(
-                radius: 90,
-                child: Icon(Icons.egg_outlined,size: 120,),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text('If you submit code directly copy from chatgpt then mark will 0',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),maxLines: 3,),
-              ),
-
-
-            ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Center(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 90,
+                  child: Icon(Icons.icecream_outlined,size: 120,),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('Ice Cream is very delicious right ?',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                CircleAvatar(
+                  radius: 90,
+                  child: Icon(Icons.code,size: 120,),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('Programming is not boring if you love it',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                CircleAvatar(
+                  radius: 90,
+                  child: Icon(Icons.egg_outlined,size: 120,),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('If you submit code directly copy from chatgpt then mark will 0',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),maxLines: 3,),
+                ),
+              ],
+            ),
           ),
         ),
       ),
